5bb39406bb16a53f6150e354d6ef41ae4a4db597
Lucas Cezario <lucasdsc96@gmail.com>
2021-08-27 21:31:13 -0300

    Return dos DTO

--- main/src/main/java/br/com/devschool/demo/domain/model/internal/dto/ProjectDTO.java
+++ main/src/main/java/br/com/devschool/demo/domain/model/internal/dto/ProjectDTO.java
@@ -1,12 +1,27 @@
 package br.com.devschool.demo.domain.model.internal.dto;
 
+import br.com.devschool.demo.domain.model.internal.Project;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
-@Builder
+
 public class ProjectDTO {
     private Integer id;
     private Boolean active;
     private String name;
+
+    public ProjectDTO(Project project){
+        this.id = project.getId();
+        this.active = project.isStatus();
+        this.name = project.getName();
+    }
+
+    public static List<ProjectDTO> convertList(List<Project> projects){
+        return projects.stream().map(ProjectDTO::new).collect(Collectors.toList());
+    }
 }
+
